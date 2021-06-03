54f266c9d74a78c2016e6e51b13833ee08de1b4e
KPAHAPK <Roman444.zaharov@gmail.com>
2021-06-03 18:28:36 +0300

    Landscape + logics

--- app/src/main/java/com/android1/calculator/Calculator.java
+++ app/src/main/java/com/android1/calculator/Calculator.java
@@ -1,4 +1,44 @@
 package com.android1.calculator;
 
-public class Calculator {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.DoubleBinaryOperator;
+
+enum Calculator {
+    ADD('+', Double::sum),
+    DIVIDE('/', (left, right) -> {
+        if (right == 0) throw new ArithmeticException("Деление на 0!");
+        return left / right;
+
+    }),
+    MULTIPLY('x', (left, right) -> left * right),
+    SUBTRACT('-', (left, right) -> left - right);
+
+    private final char symbol;
+    DoubleBinaryOperator doubleBinaryOperator;
+    public static Map<Character, Calculator> map = new HashMap<Character, Calculator>() {{
+        put(ADD.getSymbol(), ADD);
+        put(DIVIDE.getSymbol(), DIVIDE);
+        put(MULTIPLY.getSymbol(), MULTIPLY);
+        put(SUBTRACT.getSymbol(), SUBTRACT);
+
+    }};
+
+    Calculator(char symbol, DoubleBinaryOperator doubleBinaryOperator) {
+        this.symbol = symbol;
+        this.doubleBinaryOperator = doubleBinaryOperator;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public static Calculator getOperation(char operator) {
+        return map.get(operator);
+    }
+
+    public double calculate(double left, double right) {
+        return doubleBinaryOperator.applyAsDouble(left, right);
+    }
 }
+
